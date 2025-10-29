befe111749b8e404e56b3b2d3618b128147ab632
ADNAAN AHMAD <adnaanahmad28@gmail.com>
2025-10-30 02:52:15 +0530

    Login page

---
+++ Snack App/Snack App/View/login.swift
@@ -0,0 +1,18 @@
+//
+//  login.swift
+//  Snack App
+//
+//  Created by Air on 30/10/25.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+#Preview {
+    Login()
+}
